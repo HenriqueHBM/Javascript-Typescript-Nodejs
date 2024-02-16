e5654523749227af885cbc4a5f91e4c7bafe826d
HenriqueHBM <henriquemadeira1@gmail.com>
2024-02-16 12:41:54 -0300

    feat: tipos variaveis

---
+++ 002-Introdução Typescript/002-Tipos/001-String-Numbers-Bool/index.ts
@@ -0,0 +1,14 @@
+let  pedido: string = 'Pizza';
+pedido = 'Henrique';
+
+let numero: number = 123;
+numero = 322;
+
+let deubom: boolean = false;
+deubom = true;
+
+const fun = (value:string): string => { //(value:string) -> valor que vai receber, ):string -> tipof que tem que retornar
+    return value
+}
+
+console.log(fun('Henrique'));
