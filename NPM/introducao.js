6e1c4e7a97a6d5e24c82c2ac6fb74b77a9ceaf85
HenriqueHBM <henriquemadeira1@gmail.com>
2024-06-07 11:39:03 -0300

    NPM

---
+++ NPM/introducao.js
@@ -0,0 +1,7 @@
+/* 
+    -dependencies: pacotes necessarios para executar em seu projeto como producao;
+
+    -devDependencies: pacotes necessarios para executar seu projeto em um cenario
+    de desenvolvimento e testes;
+*/
+
